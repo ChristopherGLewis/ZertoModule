8a211b91fc1c5e901f9c0b341a5c9fa793de6ec5
Callum <callumslab@users.noreply.github.com>
2018-04-18 15:24:06 +0100

    Updated Connect-ZertoZVM function to include HypervisorAuth parameter

--- ZertoModule/ZertoModule.psm1
+++ ZertoModule/ZertoModule.psm1
@@ -2025,12 +2025,13 @@
         param(
             [Parameter(Mandatory=$true, HelpMessage = 'Zerto Server or ENV:\ZertoServer')] [string] $ZertoServer ,
             [Parameter(Mandatory=$false, HelpMessage = 'Zerto Server URL Port')] [string] $ZertoPort = 9669 ,
-            [Parameter(Mandatory=$false, HelpMessage  = 'User to connect to Zerto')] [string] $ZertoUser
+            [Parameter(Mandatory=$false, HelpMessage  = 'User to connect to Zerto')] [string] $ZertoUser,
+            [Parameter( Mandatory = $false, HelpMessage = 'Hypervisor manager based authentication')] [switch] $HypervisorAuth
         )
         
         Set-Item ENV:ZertoServer $ZertoServer
         Set-Item ENV:ZertoPort  $ZertoPort 
-        Set-Item ENV:ZertoToken ((Get-ZertoAuthToken -ZertoServer $ZertoServer -ZertoPort $ZertoPort -ZertoUser $ZertoUser) | ConvertTo-Json -Compress) 
+        Set-Item ENV:ZertoToken ((Get-ZertoAuthToken @PSBoundParameters) | ConvertTo-Json -Compress) 
         Set-Item ENV:ZertoVersion (Get-ZertoLocalSite).version
     }
 
